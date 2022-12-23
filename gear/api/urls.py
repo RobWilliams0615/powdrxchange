b61eac0bcf7ca36b77384d6541e2e06a2eb34eee
Rob Williams <92275209+RobWilliams0615@users.noreply.github.com>
2022-12-22 17:01:18 -0700

    updated views to class based and updated urls

--- gear/api/urls.py
+++ gear/api/urls.py
@@ -1,10 +1,9 @@
 
 from django.urls import path, include
-from gear.api.views import gear_list, gear_details
+# from gear.api.views import gear_list, gear_details
+from gear.api.views import GearDetailAV, GearListAV
 
 urlpatterns = [
-  
-    path('list/', gear_list, name='gear-list'),
-    path('<int:pk>', gear_details, name='gear-details'),
-
+    path('list/', GearListAV.as_view(), name='gear-list'),
+    path('<int:pk>', GearDetailAV.as_view(), name='gear-details'),
 ]
--- gear/api/views.py
+++ gear/api/views.py
@@ -2,34 +2,38 @@ from gear.models import Gear
 from rest_framework import status
 from gear.api.serializers import GearSerializer
 from rest_framework.response import Response
-from rest_framework.decorators import api_view
+from rest_framework.views import APIView
+# from rest_framework.decorators import api_view
 
+class GearListAV(APIView):
 
-@api_view(['GET', 'POST'])
-def gear_list(request):
-  if request.method == 'GET':
+  def get(self, request):
     gears = Gear.objects.all()
     serializer = GearSerializer(gears, many=True)
-    return Response(serializer.data, status=status.HTTP_200_OK)
+    return Response(serializer.data)
 
-  if request.method == 'POST':
+  def post(self, request):
     serializer = GearSerializer(data=request.data)
     if serializer.is_valid():
         serializer.save()
         return Response(serializer.data)
     else:
       return Response(serializer.errors, status=status.HTTP_201_CREATED)
-  
 
-@api_view(['GET', 'PUT', 'DELETE'])
-def gear_details(request, pk):
 
-  if request.method == 'GET':
-    gear = Gear.objects.get(pk=pk)
+
+class GearDetailAV(APIView):
+
+  def get(self, request, pk):
+    try:
+      gear = Gear.objects.get(pk=pk)
+    except Gear.DoesNotExist:
+      return Response(status=status.HTTP_404_NOT_FOUND)
+
     serializer = GearSerializer(gear)
     return Response(serializer.data)
 
-  if request.method == 'PUT':
+  def put(self, request, pk):
     gear = Gear.objects.get(pk=pk)
     serializer = GearSerializer(gear, data=request.data)
     if serializer.is_valid():
@@ -38,8 +42,49 @@ def gear_details(request, pk):
     else:
       return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+  def delete(self, request, pk):
+    if request.method == 'DELETE':
+      gear = Gear.objects.get(pk=pk)
+      gear.delete()
+      return Response(status=status.HTTP_204_NO_CONTENT)
 
-  if request.method == 'DELETE':
-    gear = Gear.objects.get(pk=pk)
-    gear.delete()
-    return Response(status=status.HTTP_204_NO_CONTENT)
+# function baes views below 
+
+# @api_view(['GET', 'POST'])
+# def gear_list(request):
+#   if request.method == 'GET':
+#     gears = Gear.objects.all()
+#     serializer = GearSerializer(gears, many=True)
+#     return Response(serializer.data, status=status.HTTP_200_OK)
+
+#   if request.method == 'POST':
+#     serializer = GearSerializer(data=request.data)
+#     if serializer.is_valid():
+#         serializer.save()
+#         return Response(serializer.data)
+#     else:
+#       return Response(serializer.errors, status=status.HTTP_201_CREATED)
+  
+
+# @api_view(['GET', 'PUT', 'DELETE'])
+# def gear_details(request, pk):
+
+#   if request.method == 'GET':
+#     gear = Gear.objects.get(pk=pk)
+#     serializer = GearSerializer(gear)
+#     return Response(serializer.data)
+
+#   if request.method == 'PUT':
+#     gear = Gear.objects.get(pk=pk)
+#     serializer = GearSerializer(gear, data=request.data)
+#     if serializer.is_valid():
+#       serializer.save()
+#       return Response(serializer.data)
+#     else:
+#       return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+#   if request.method == 'DELETE':
+#     gear = Gear.objects.get(pk=pk)
+#     gear.delete()
+#     return Response(status=status.HTTP_204_NO_CONTENT)
