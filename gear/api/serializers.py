5765327ece23bfcb0d9cf45485395b133afc424f
Rob Williams <92275209+RobWilliams0615@users.noreply.github.com>
2022-12-23 08:49:00 -0700

    gear serializer chnged to model serializer, validate functions updated

--- gear/api/serializers.py
+++ gear/api/serializers.py
@@ -1,32 +1,42 @@
 from rest_framework import serializers
 from gear.models import Gear
 
-def name_length(value):
-    if len(value) < 5:
-      raise serializers.ValidationError('Gear name must be at least 2 characters.')
-    
 
-class GearSerializer(serializers.Serializer):
-  id = serializers.IntegerField(read_only=True)
-  name = serializers.CharField(validators=[name_length])
-  description = serializers.CharField()
-  price = serializers.IntegerField()
-  active = serializers.BooleanField()
+class GearSerializer(serializers.ModelSerializer):
 
-  def create(self, validated_data):
-    return Gear.objects.create(**validated_data)
+    class Meta:
+      model = Gear
+      fields = "__all__"
 
-  def update(self, instance, validated_data):
-    instance.name = validated_data.get('name', instance.name)
-    instance.description = validated_data.get('description', instance.description)
-    instance.price = validated_data.get('price', instance.price)
-    instance.active = validated_data.get('active', instance.active)
-    instance.save()
-    return instance
+    def validate_name(self, value):
+        if len(value) < 5:
+          raise serializers.ValidationError('Gear name must be at least 2 characters.')
+        else:
+          return value
+      
+    def validate(self, data):
+      if data['name'] == data['description']:
+        raise serializers.ValidationError('Gear name cannot be same as description.')
+      else:
+        return data 
 
-  def validate(self, data):
-    if data['name'] == data['description']:
-      raise serializers.ValidationError('Gear name cannot be same as description.')
-    else:
-      return data 
+# class GearSerializer(serializers.Serializer):
+#   id = serializers.IntegerField(read_only=True)
+#   name = serializers.CharField(validators=[name_length])
+#   description = serializers.CharField()
+#   price = serializers.IntegerField()
+#   active = serializers.BooleanField()
+
+#   def create(self, validated_data):
+#     return Gear.objects.create(**validated_data)
+
+#   def update(self, instance, validated_data):
+#     instance.name = validated_data.get('name', instance.name)
+#     instance.description = validated_data.get('description', instance.description)
+#     instance.price = validated_data.get('price', instance.price)
+#     instance.active = validated_data.get('active', instance.active)
+#     instance.save()
+#     return instance
+
+#   
   
