15c9585c11a31c65eedd843d80d210480a610a98
Rob Williams <92275209+RobWilliams0615@users.noreply.github.com>
2023-01-07 14:04:52 -0700

    added url path for reviewlist view and tested, all reviews show

--- gear/api/urls.py
+++ gear/api/urls.py
@@ -1,11 +1,12 @@
 
 from django.urls import path, include
 # from gear.api.views import gear_list, gear_details
-from gear.api.views import GearDetailAV, GearListAV, GearPlatFormListAV, GearPlatFormDetailAV
+from gear.api.views import GearDetailAV, GearListAV, GearPlatFormListAV, GearPlatFormDetailAV, GearReviewList
 
 urlpatterns = [
     path('list/', GearListAV.as_view(), name='gear-list'),
     path('store/', GearPlatFormListAV.as_view(), name='store-list'),
     path('<int:pk>', GearDetailAV.as_view(), name='gear-details'),
     path('store/<int:pk>', GearPlatFormDetailAV.as_view(), name='store-details'),
+    path('review', GearReviewList.as_view(), name='gear-reviews'),
 ]
