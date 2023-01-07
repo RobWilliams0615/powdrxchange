bd7fa72dca6adf8e4d1388b5cc6feacaba31cbdd
Rob Williams <92275209+RobWilliams0615@users.noreply.github.com>
2023-01-07 14:19:21 -0700

    added review detail generic view and added review detail url, tested and working

--- gear/api/urls.py
+++ gear/api/urls.py
@@ -1,7 +1,7 @@
 
 from django.urls import path, include
 # from gear.api.views import gear_list, gear_details
-from gear.api.views import GearDetailAV, GearListAV, GearPlatFormListAV, GearPlatFormDetailAV, GearReviewList
+from gear.api.views import GearDetailAV, GearListAV, GearPlatFormListAV, GearPlatFormDetailAV, GearReviewList, ReviewDetail
 
 urlpatterns = [
     path('list/', GearListAV.as_view(), name='gear-list'),
@@ -9,4 +9,5 @@ urlpatterns = [
     path('<int:pk>', GearDetailAV.as_view(), name='gear-details'),
     path('store/<int:pk>', GearPlatFormDetailAV.as_view(), name='store-details'),
     path('review', GearReviewList.as_view(), name='gear-reviews'),
+    path('review/<int:pk>', ReviewDetail.as_view(), name='gear-reviews-details'),
 ]
--- gear/api/views.py
+++ gear/api/views.py
@@ -13,18 +13,24 @@ from rest_framework import mixins
 # from rest_framework.decorators import api_view
 
 ## Reviews ##
-
-class GearReviewList(mixins.ListModelMixin,
-                  mixins.CreateModelMixin,
-                  generics.GenericAPIView):
+class ReviewDetail(mixins.RetrieveModelMixin, generics.GenericAPIView):
     queryset = Review.objects.all()
     serializer_class = ReviewSerializer
 
     def get(self, request, *args, **kwargs):
-        return self.list(request, *args, **kwargs)
+      return self.retrieve(request, *args, **kwargs)
 
-    def post(self, request, *args, **kwargs):
-        return self.create(request, *args, **kwargs)
+class GearReviewList(mixins.ListModelMixin,
+                mixins.CreateModelMixin,
+                generics.GenericAPIView):
+  queryset = Review.objects.all()
+  serializer_class = ReviewSerializer
+
+  def get(self, request, *args, **kwargs):
+      return self.list(request, *args, **kwargs)
+
+  def post(self, request, *args, **kwargs):
+      return self.create(request, *args, **kwargs)
 
 ## Product List ##
 
