7085c2c7b4e329fde7f76670bdb71de3d899193d
Rob Williams <92275209+RobWilliams0615@users.noreply.github.com>
2023-01-28 08:46:35 -0700

    added temporary url for built in login form to use for permissions testing

--- powdrxchange/urls.py
+++ powdrxchange/urls.py
@@ -5,6 +5,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('gear/', include('gear.api.urls')),
+    path('api-auth', include('rest_framework.urls')),
 
 
 ]
