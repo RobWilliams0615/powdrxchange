8b2d443b5c67ffa0bc263242dd86f80646c13029
Rob Williams <92275209+RobWilliams0615@users.noreply.github.com>
2023-01-29 13:50:16 -0700

    updated AdminOrReadOnly permission with same logic to check if the request is get then they can acess, if the request is not get then it checks to make sure the user is staff/admin

--- gear/api/permissions.py
+++ gear/api/permissions.py
@@ -3,9 +3,10 @@ from rest_framework import permissions
 class AdminorReadOnly(permissions.IsAdminUser):
 
     def has_permission(self, request, view):
-      admin_permission = bool(request.user and request.user.is_staff)
-      return request.method == "GET" or admin_permission
-
+        if request.method in permissions.SAFE_METHODS:
+              return True
+        else:
+              return bool(request.user and request.user.is_staff)
 
 class ReviewerOrReadOnly(permissions.BasePermission):
 
