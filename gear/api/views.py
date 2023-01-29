76b61a542fb1890f1ae775873ef12e85726eca09
Rob Williams <92275209+RobWilliams0615@users.noreply.github.com>
2023-01-29 12:55:54 -0700

    updated permissions class to IsAuthenticatedOrReadOnly for reviewlist and review detail so users can read the reviews but not edit them

--- gear/api/views.py
+++ gear/api/views.py
@@ -12,7 +12,7 @@ from rest_framework import generics
 from rest_framework import mixins
 from rest_framework import viewsets
 from rest_framework.exceptions import ValidationError
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from django.shortcuts import get_object_or_404
 
 ## Reviews ##
@@ -40,12 +40,12 @@ class GearReviewCreate(generics.CreateAPIView):
 class GearReviewDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Review.objects.all()
     serializer_class = ReviewSerializer
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticatedOrReadOnly]
 
 
 class GearReviewList(generics.ListAPIView):
     serializer_class = ReviewSerializer
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticatedOrReadOnly]
 
     def get_queryset(self):
       pk = self.kwargs['pk']
