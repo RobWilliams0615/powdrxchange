00d4595e414ae8bcafea81abcc8c1e4a5a970e71
Rob Williams <92275209+RobWilliams0615@users.noreply.github.com>
2023-01-29 13:16:49 -0700

    removed unused mixins import and isauthetnicated import

--- gear/api/views.py
+++ gear/api/views.py
@@ -9,7 +9,6 @@ from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework import generics
-from rest_framework import mixins
 from rest_framework import viewsets
 from rest_framework.exceptions import ValidationError
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
