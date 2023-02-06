8ee02f260b19beba98fa66370c93af51d69c61e5
Rob Williams <92275209+RobWilliams0615@users.noreply.github.com>
2023-02-06 08:50:51 -0700

    added a save function on self, then added validation to make sure passwords match, email has not already been used, tested in postman

--- user/api/serializers.py
+++ user/api/serializers.py
@@ -13,3 +13,20 @@ class Register(serializers.ModelSerializer):
       }
 
 
+    def save(self):
+
+        password = self.validated_data['password']
+        password2 = self.validated_data['password2']
+
+        if password != password2:
+            raise serializers.ValidationError({'error': 'Passwords need to match'})
+
+
+        if User.objects.filter(email=self.validated_data['email']).exists():
+            raise serializers.ValidationError({'error': 'This email is already used'})
+
+
+        account = User(email=self.validated_data['email'], username=self.validated_data['username'])
+        account.set_password(password)
+
+
