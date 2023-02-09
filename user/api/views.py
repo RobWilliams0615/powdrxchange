c41aad5c02a5e5a888b4d50f5ffb31c0503e5e16
Rob Williams <92275209+RobWilliams0615@users.noreply.github.com>
2023-02-09 09:05:53 -0700

    imported status from rest framework. created a logout function that checks current user and deletes token upon logout and returns a 200 respomnse if succesful

--- user/api/views.py
+++ user/api/views.py
@@ -3,6 +3,16 @@ from user.api.serializers import Register
 from rest_framework.response import Response
 from rest_framework.authtoken.models import Token
 from user.api.models import create_auth_token
+from rest_framework import status
+
+
+
+@api_view(['POST',])
+def logout_view(request):
+
+    if request.method == 'POST':
+        request.user.auth_token.delete()
+        return Response(status=status.HTTP_200_OK)
 
 @api_view(['POST'])
 def register_view(request):
